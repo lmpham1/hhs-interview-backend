903fe4039081840f7ac52f568d3073393fd3ec7c
Minh Pham <est2000vn@gmail.com>
2022-12-06 16:19:02 -0500

    First commit, created health check route

---
+++ src/routes/api/index.js
@@ -0,0 +1,11 @@
+// src/routes/api/index.js
+
+/**
+ * The main entry-point for the v1 version of the fragments API.
+ */
+ const express = require('express');
+
+ // Create a router on which to mount our API endpoints
+ const router = express.Router();
+ 
+ module.exports = router;
